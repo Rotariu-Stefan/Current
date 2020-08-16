372c947e09a04b66de064ad543b5fd2394c9ed25
Rotariu Stefan <62540642+Rotariu-Stefan@users.noreply.github.com>
2020-08-16 18:26:19 +0300

    lc daily better

--- LeetCode/MaxProfit.js
+++ LeetCode/MaxProfit.js
@@ -1,6 +1,5 @@
 //https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/551/week-3-august-15th-august-21st/3426/
-
-/** O(n) for 1Transaction
+/** O(n) sO(n) n=nr. of days/prices
  * @param {number[]} prices
  * @return {number}
  */
@@ -9,12 +8,67 @@ var maxProfit = function(prices) {
         return 0;
     }
 
-    const mid=Math.floor(prices.length-1/2);
-    const maxProfit=getBestMM(prices,
-                              getMinMax(prices, 0, mid),
-                              getMinMax(prices, mid+1, prices.length-1));
-    return prices[maxProfit.max]-prices[maxProfit.min];
+    const memo=[];
+    memo.length=prices.length;
+    memo[0]=0;
+
+    let min=prices[0];
+    let max=prices[0];
+    let profit=0;
+    for(let i=1;i<prices.length;i++){
+        if(prices[i]-min>profit){
+            max=prices[i];
+            profit=max-min;
+        }
+        if(prices[i]<min){
+            min=prices[i];
+            max=prices[i];
+        }
+        memo[i]=profit;
+    }
+
+    min=prices[prices.length-1];
+    max=prices[prices.length-1];
+    profit=0;
+    let maxProfit=0;
+    for(let i=prices.length-1;i>=0;i--){
+        if(max-prices[i]>profit){
+            min=prices[i];
+            profit=max-min;
+        }
+        if(prices[i]>max){
+            max=prices[i];
+            min=prices[i];
+        }
+        if(memo[i]+profit>maxProfit){
+            maxProfit=memo[i]+profit;
+        }
+    }
+
+    return maxProfit;
 };
+
+
+/** O(n^2) sO(1-stack) n=nr. prices
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+     if(prices.length<2){
+         return 0;
+     }
+
+     let maxProfit=0;
+     for(let i=0;i<prices.length;i++){
+         const mm1=getMinMax(prices,0,i);
+         const mm2=getMinMax(prices,i+1,prices.length-1);
+         const profit=mm1.max-mm1.min+mm2.max-mm2.min;
+         if(profit>maxProfit){
+             maxProfit=profit;
+         }
+     }
+     return maxProfit;
+ };
 
 const getBestMM = (prices, mm1, mm2) =>{
     const profit1=prices[mm1.max]-prices[mm1.min];
@@ -43,13 +97,6 @@ const getMinMax = (prices, start, end) =>{
     const mid=Math.floor((end+start)/2);
     return getBestMM(prices, getMinMax(prices, start, mid), getMinMax(prices, mid+1, end));
 };
-
-
-
-
-
-
-
 
 
 /** O(n^3) sO(1) n=nr. of days/prices
